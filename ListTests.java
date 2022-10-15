07c219329ee5f2cb3b809ca486b6f62fe2b94e7e
sichengjing <sjing@ucsd.edu>
2022-10-14 19:12:36 -0700

    Update ListTests.java

--- ListTests.java
+++ ListTests.java
@@ -1,13 +1,15 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class ListTests{
-  @Test
-  public void testfilter(){
-    String[] contents1 = {"a1", "b", "a2", "a3", "c"};
-    String[] contents2 = {"a1", "a2", "a3"};
-    List<String> list1 = new ArrayList<>(contents1);
-    List<String> list2 = new ArrayList<>(contents2);
-    assertArrayEquals(list2, ListExamples.filter(list1, sc.filter(new checka())));
-  }
+import java.util.Arrays;
+import java.util.List;
+
+@Test
+public void testfilter() {
+    String[] expect = new String[]{"Linked", "Array"};
+    List<String> test = Arrays.asList("Linked", "Array");
+    List<String> filter = ListExamples.filter(test, new AllTrueFilter());
+    String[] objects = filter.size() == 0 ? new String[]{} : filter.toArray(new String[filter.size()]);
+    assertArrayEquals(expect, objects);
 }
+
